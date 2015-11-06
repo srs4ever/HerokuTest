d5ca1326156476ba2ae515deda082a16213838b6
scheekat <srinivas.cheekatla@capgemini.com>
2015-11-06 10:50:34 +0530

    servlet Comment

--- HerokuTest/src/main/resources/com/HerokuServlet.java
+++ HerokuTest/src/main/resources/com/HerokuServlet.java
@@ -14,24 +14,24 @@ public class HerokuServlet extends HttpServlet {
        
     /**
      * @see HttpServlet#HttpServlet()
-     */
+     *//*
     public HerokuServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
+	*//**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 *//*
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
-	/**
+	*//**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 *//*
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
-
+*/
 }
